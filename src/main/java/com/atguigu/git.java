7e8ccfe2a0c8b73b092c24417a08fa094f32bb26
Root <Root@qq.com>
2022-12-12 20:04:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+/**
+ * @author Wan
+ * @Description TODO
+ * @create 2022-12-12 19:59
+ * @Version 1.0
+ */
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
