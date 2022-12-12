933bae72cdae4acf865a379ba8bf411e47f35025
Root <Root@qq.com>
2022-12-12 20:08:38 +0800

    second commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -12,8 +12,9 @@ package com.atguigu;
 public class git {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
 
     }
 }
