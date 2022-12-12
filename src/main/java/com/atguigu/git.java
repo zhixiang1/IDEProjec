44d0e369231e526a2f40f16481dbdedc9c8997de
Root <Root@qq.com>
2022-12-12 20:23:25 +0800

    master commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -16,6 +16,7 @@ public class git {
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
         System.out.println("Hello hot-fix!");
+        System.out.println("master test");
 
 
     }
