29ae050886baa804b6c9ddca27c0cc6ada5f050c
Root <Root@qq.com>
2022-12-12 20:08:11 +0800

    second commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -3,6 +3,9 @@ package com.atguigu;
 /**
  * @author Wan
  * @Description TODO
+ *                  1.初始化本地库
+ *                  2.添加暂存区
+ *                  3.提交本地库
  * @create 2022-12-12 19:59
  * @Version 1.0
  */
@@ -10,5 +13,7 @@ public class git {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
+
     }
 }
